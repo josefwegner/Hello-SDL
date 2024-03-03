eb848fc6c116518243578577efac44aca6bc1a84
Josef Wegner <josef.wegner@outlook.com>
2024-03-03 14:04:17 +0100

    Added OpenGL

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,84 @@
+/**
+ * Main.cpp
+ */
+
+#include <cstdio>
+#include <cstdlib>
+
+#ifndef AMIGAOS4
+#define GL_GLEXT_PROTOTYPES
+#endif
 #include <SDL.h>
-#include "Window.h"
+#include <SDL_opengles2.h>
 
-int main() {
-  Window GameWindow;
-  SDL_Event event;
-  bool quit = false;
+int main(int argc, char *argv[])
+{
+  // Initialization
+  const int screenWidth = 1280;
+  const int screenHeight = 768;
+  const char *windowTitle = "Hello world!";
 
-  // Application Loop
-  while (true) {
-    // Event Loop
-    while (SDL_PollEvent(&event)) {
-      if (event.type == SDL_QUIT) [[unlikely]] {
-        SDL_Quit();
-        return 0;
-      }
-    }
-    GameWindow.RenderFrame();
+  // The window
+  SDL_Window *window = NULL;
+
+  // The OpenGL context
+  SDL_GLContext context = NULL;
+
+  // Init SDL
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    fprintf(stderr, "SDL could not initialize! SDL_Error: %s\n",
+            SDL_GetError());
+
+    return EXIT_FAILURE;
   }
 
-  return 0;
+  // Setup the exit hook
+  atexit(SDL_Quit);
+
+  // Request OpenGL 3.0
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
+  // Want double-buffering
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+  // Create the window
+  window = SDL_CreateWindow(windowTitle, SDL_WINDOWPOS_UNDEFINED,
+                            SDL_WINDOWPOS_UNDEFINED, screenWidth, screenHeight,
+                            SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+
+  if (!window) {
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error",
+                             "Couldn't create the main window.", NULL);
+    return EXIT_FAILURE;
+  }
+
+  context = SDL_GL_CreateContext(window);
+  if (!context) {
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error",
+                             "Couldn't create an OpenGL context.", NULL);
+
+    return EXIT_FAILURE;
+  }
+
+  // Clear to black
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  // Update the window
+  SDL_GL_SwapWindow(window);
+
+  // Wait for the user to quit
+  bool quit = false;
+  while (!quit) {
+    SDL_Event event;
+    if (SDL_WaitEvent(&event) != 0) {
+      if (event.type == SDL_QUIT) {
+        // User wants to quit
+        quit = true;
+      }
+    }
+  }
+
+  return EXIT_SUCCESS;
 }
